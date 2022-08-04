eb461f26d2405c119b4461b2ce70a9bbd0cc39bf
rhianlandim <rhiandl.moura@gmail.com>
2022-08-04 20:13:52 -0300

    project finished

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Main from "./pages/Main";
+import Repositorio from "./pages/Repositorio";
+
+export default function Rotas () {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Main}/>
+                <Route exact path="/repositorio/:repositorio" component={Repositorio}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
